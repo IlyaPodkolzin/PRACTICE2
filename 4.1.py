41983c91ed927ea909da91d1c6da5cb959621a24
IlyaMorozov <pdk.04@mail.ru>
2024-03-07 13:56:03 +0300

    +4.1

--- 4.1.py
+++ 4.1.py
@@ -1,7 +1,7 @@
 def generate_groups():
     group_dict = {"В": 9, "К": 37, "М": 2, "Н": 12}
     for key in group_dict.keys():
-        print('\n'.join([f'И{key}БО', " ".join([f'И{key}БО-0{val + 1}-22' if val < 11 else f'И{key}БО-{val + 1}-22' for val in range(group_dict[key])])]))
+        print('\n'.join([f'И{key}БО', " ".join([f'И{key}БО-0{val + 1}-22' if val < 9 else f'И{key}БО-{val + 1}-22' for val in range(group_dict[key])])]))
 
 
 generate_groups()
